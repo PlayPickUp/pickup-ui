435a32e13862ce855e2773770683355df07169e7
Eric Stout <ericwstout@gmail.com>
2021-01-21 13:33:33 -0600

    Update exports and main js file

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,11 @@
+// export { default as Button } from "./Button";
+// export { default as ThemeProvider } from "./ThemeProvider";
+// export { default as Typography } from "./Typography";
+
+// import Button from './Button';
+
 export { default as Button } from "./Button";
+export * from "./Button";
+
 export { default as ThemeProvider } from "./ThemeProvider";
-export { default as Typography } from "./Typography";
+export * from "./ThemeProvider";
