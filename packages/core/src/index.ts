044624c339aedaad0218e9d9f059bec92a328623
Eric Stout <ericwstout@gmail.com>
2021-01-26 17:06:39 -0600

    Fix typo in letterSpacing

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,5 @@
-// export { default as Button } from "./Button";
-// export { default as ThemeProvider } from "./ThemeProvider";
-// export { default as Typography } from "./Typography";
-
 import Button from "./Button";
 import ThemeProvider from "./ThemeProvider";
 import Typography from './Typography';
 
 export { Button, ThemeProvider, Typography };
-// export { default as Button } from "./Button";
-// export { default as ThemeProvider } from "./ThemeProvider";
