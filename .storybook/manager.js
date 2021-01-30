016b66ef8786ca9b943122ddc348af7c97014b57
Eric Stout <ericwstout@gmail.com>
2021-01-30 00:11:18 -0600

    Add initial storybook setup and Typography component

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import pickupTheme from "./pickup-theme";
+
+addons.setConfig({
+  theme: pickupTheme,
+});
---
+++ stories/Typography.stories.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import { Typography } from "@playpickup/core";
+import { TypographyProps } from "@playpickup/core/src/types";
+
+export default {
+  title: "Core/Typography",
+  component: Typography,
+  argTypes: {
+    color: { control: "color" },
+    variant: {
+      defaultValue: "body",
+      description: "Text style and element to be used",
+      control: {
+        type: "select",
+        options: [
+          "title",
+          "body",
+          "heading2",
+          "heading3",
+          "heading4",
+          "heading5",
+          "heading6",
+          "span",
+        ],
+      },
+    },
+  },
+  args: {
+    children: "PickUp",
+    variant: "title",
+    className: null,
+    style: null,
+    element: null,
+    color: "#2C2933",
+  },
+  parameters: {
+    variant: [
+      "title",
+      "body",
+      "heading2",
+      "heading3",
+      "heading4",
+      "heading5",
+      "heading6",
+      "span",
+    ],
+  },
+} as Meta;
+
+const Template: Story<TypographyProps> = (args) => <Typography {...args} />;
+
+export const Default = Template.bind({});
