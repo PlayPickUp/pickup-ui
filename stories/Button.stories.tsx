ef460a88a13454795f708149c827f95a780c93ab
Eric Stout <ericwstout@gmail.com>
2021-02-01 14:08:22 -0600

    Add button stories and deploy script

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,38 +1,79 @@
-import React from 'react';
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import React from "react";
+import { Story, Meta } from "@storybook/react";
 
-import { Button, ButtonProps } from './Button';
+import { Button } from "@playpickup/core";
+import { ButtonProps } from "@playpickup/core/src/types";
 
 export default {
-  title: 'Example/Button',
+  title: "Core/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
+    children: {
+      defaultValue: null,
+      description: "Text to be displayed",
+    },
+    className: {
+      defaultValue: null,
+      description: "Allows for className additions",
+    },
+    disabled: {
+      defaultValue: false,
+      description: "Toggles disabled state",
+    },
+    element: {
+      defaultValue: "button",
+      description: "Allows for overriding of the core HTML element",
+    },
+    href: {
+      defaultValue: null,
+      description: "Adds href property to button",
+    },
+    onClick: {
+      defaultValue: null,
+      description: "Function to fire when clicked",
+    },
+    style: {
+      defaultValue: null,
+      description: "CSS style object to pass to component",
+    },
+    to: {
+      defaultValue: null,
+      description:
+        "Allows the button to use React Router Link component under the hood and uses value as link",
+    },
+    variant: {
+      defaultValue: "fit",
+      description: "Button style variant",
+    },
+    useSubmit: {
+      defaultValue: false,
+      description: "Overrides inner component to use a submit input",
+    },
+    submitText: {
+      defaultValue: null,
+      description: "Submit button text, only used when using 'useSubmit'",
+    },
+  },
+  args: {
+    children: "Make your pick",
+    className: "",
+    disabled: false,
+    element: "button",
+    href: "",
+    onClick: null,
+    style: null,
+    to: "",
+    variant: "fit",
+    useSubmit: false,
+    submitText: "",
   },
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+export const Fit = Template.bind({});
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Full = Template.bind({});
+Full.args = {
+  variant: "full",
 };
