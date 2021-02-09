9837ee7aa9a4f562af9cb5a2acc4dc6b263110e4
Eric Stout <ericwstout@gmail.com>
2021-02-08 21:31:04 -0600

    Add icon wrapper and switch to streamline icons

--- packages/icons/src/icon/Create.tsx
+++ packages/icons/src/icon/Create.tsx
@@ -1,17 +1,63 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import { IconProps } from "../types";
 
 const Create: React.FC<IconProps> = ({ className, color, style }) => {
-  return (
-    <FontAwesomeIcon
-      className={className}
-      icon={faPlus}
-      style={{ ...style, color: color || null }}
-    />
-  );
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			version="1.1"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			viewBox="0 0 140 140"
+			width="140"
+			height="140"
+			className={className}
+			style={style}
+		>
+			<g transform="matrix(5.833333333333333,0,0,5.833333333333333,0,0)">
+				<path
+					d="M22.627,14.87,15,22.5l-3.75.75L12,19.5l7.631-7.63a2.113,2.113,0,0,1,2.991,0l.009.008A2.116,2.116,0,0,1,22.627,14.87Z"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+				<path
+					d="M8.246,20.25h-6a1.5,1.5,0,0,1-1.5-1.5V2.25a1.5,1.5,0,0,1,1.5-1.5h15a1.5,1.5,0,0,1,1.5,1.5V9"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+				<path
+					d="M8.246 5.25L14.246 5.25"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+				<path
+					d="M5.246 9.75L14.246 9.75"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+				<path
+					d="M5.246 14.25L12.746 14.25"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+			</g>
+		</svg>
+	);
 };
 
 export default Create;
--- packages/icons/src/icon/List.tsx
+++ packages/icons/src/icon/List.tsx
@@ -1,16 +1,69 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faList } from "@fortawesome/free-solid-svg-icons";
-
 import { IconProps } from "../types";
 
 const List: React.FC<IconProps> = ({ className, color, style }) => {
 	return (
-		<FontAwesomeIcon
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			version="1.1"
+			xmlnsXlink="http://www.w3.org/1999/xlink"
+			viewBox="0 0 140 140"
+			width="140"
+			height="140"
 			className={className}
-			icon={faList}
-			style={{ ...style, color: color || null }}
-		/>
+			style={style}
+		>
+			<g transform="matrix(5.833333333333333,0,0,5.833333333333333,0,0)">
+				<path
+					d="M8.25 3.748L23.25 3.748"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+				<path
+					d="M8.25 12.748L23.25 12.748"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+				<path
+					d="M8.25 21.748L23.25 21.748"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+				<path
+					d="M0.750 0.748 L5.250 0.748 L5.250 5.248 L0.750 5.248 Z"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+				<path
+					d="M0.750 9.748 L5.250 9.748 L5.250 14.248 L0.750 14.248 Z"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+				<path
+					d="M0.750 18.748 L5.250 18.748 L5.250 23.248 L0.750 23.248 Z"
+					fill="none"
+					stroke={color}
+					strokeLinecap="round"
+					strokeLinejoin="round"
+					strokeWidth="1.5"
+				></path>
+			</g>
+		</svg>
 	);
 };
 
