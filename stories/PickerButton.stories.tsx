fb099b96e22c99d8401dca622ef29ea83c129091
Eric Stout <ericwstout@gmail.com>
2021-07-11 15:30:28 -0500

    Merge pull request #73 from PlayPickUp/feature/picker-button-styles
    feat(PickerButton): Add different renders for PickerButton to show re…

--- stories/PickerButton.stories.tsx
+++ stories/PickerButton.stories.tsx
@@ -24,12 +24,44 @@ export default {
       defaultValue: null,
       description: "Text to be displayed on inside the button",
     },
+    onClick: {
+      defaultValue: null,
+      description: "On click handler",
+    },
+    showResult: {
+      defaultValue: false,
+      description: "Switches the component to show results of pick",
+    },
+    result: {
+      defaultValue: null,
+      description: "Result of the pick popularity",
+    },
+    isPick: {
+      defaultValue: false,
+      description:
+        "Switches component to show itself as the current selection (or pick)",
+    },
   },
   args: {
     displayText: "Absolutely 💯",
+    onClick: (e) => console.log(e),
+    isPick: false,
+    result: 33.33,
   },
 } as Meta;
 
 const Template: Story<PickerButtonProps> = (args) => <PickerButton {...args} />;
 
 export const Default = Template.bind({});
+export const Result = Template.bind({});
+export const SelectedPick = Template.bind({});
+
+Result.args = {
+  isPick: false,
+  showResult: true,
+};
+
+SelectedPick.args = {
+  isPick: true,
+  showResult: true,
+};
