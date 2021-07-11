9ad1b35582263f7e2783c37c52cc7c7e3cfac57e
Eric Stout <ericwstout@gmail.com>
2021-07-11 15:27:39 -0500

    chore(tests): Add better tests for PickerButton

--- packages/core/src/PickerButton/PickerButton.test.tsx
+++ packages/core/src/PickerButton/PickerButton.test.tsx
@@ -9,7 +9,12 @@ const handleClick = jest.fn();
 test("Renders and matches snapshot", () => {
   const { container } = render(
     <ThemeProvider>
-      <PickerButton displayText="Absolutely" onClick={handleClick} />
+      <PickerButton
+        result={74}
+        displayText="Absolutely"
+        isPick={false}
+        onClick={handleClick}
+      />
     </ThemeProvider>
   );
   expect(container).toMatchSnapshot();
@@ -18,7 +23,12 @@ test("Renders and matches snapshot", () => {
 test("Display text renders correctly", () => {
   const { getByText } = render(
     <ThemeProvider>
-      <PickerButton displayText="Absolutely" onClick={handleClick} />
+      <PickerButton
+        isPick={false}
+        result={85}
+        displayText="Absolutely"
+        onClick={handleClick}
+      />
     </ThemeProvider>
   );
   expect(getByText("Absolutely")).toBeTruthy();
@@ -27,9 +37,30 @@ test("Display text renders correctly", () => {
 test("On click fires correctly", () => {
   const { getByTestId } = render(
     <ThemeProvider>
-      <PickerButton displayText="Absolutey" onClick={handleClick} />
+      <PickerButton
+        result={34}
+        displayText="Absolutey"
+        onClick={handleClick}
+        isPick={false}
+      />
     </ThemeProvider>
   );
   fireEvent.click(getByTestId("picker-button"));
   expect(handleClick).toHaveBeenCalledTimes(1);
+});
+
+test("Display of results text renders correctly", () => {
+  const { getByText } = render(
+    <ThemeProvider>
+      <PickerButton
+        showResult={true}
+        isPick={true}
+        result={85}
+        displayText="Absolutely"
+        onClick={handleClick}
+      />
+    </ThemeProvider>
+  );
+  expect(getByText("Absolutely")).toBeTruthy();
+  expect(getByText("85%")).toBeTruthy();
 });
