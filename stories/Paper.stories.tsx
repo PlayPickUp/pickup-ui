59fb85608ed6e1f199e882fcab4f5711e05fb169
Eric Stout <ericwstout@gmail.com>
2021-07-30 16:17:21 -0400

    chore(docs): Add Paper documentation, close #69

---
+++ stories/Paper.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import { Paper, PaperProps } from "@playpickup/core";
+
+export default {
+  title: "Core/Paper",
+  component: Paper,
+  argTypes: {
+    className: {
+      defaultValue: null,
+      description: "Allows for className additions or overrides",
+      control: "text",
+      table: {
+        category: "Styling",
+      },
+    },
+    style: {
+      defaultValue: null,
+      description: "Allows for CSS Styles to be applied",
+      control: "object",
+      table: {
+        category: "Styling",
+      },
+    },
+    padding: {
+      defaultValue: null,
+      description: "Applies a padding value to the component",
+      table: {
+        category: "Styling",
+      },
+    },
+    children: {
+      type: { required: true },
+      defaultValue: null,
+      description: "Content/children for the component",
+    },
+  },
+  args: {
+    children: <div>Hello, World</div>,
+    padding: 20,
+  },
+} as Meta;
+
+const Template: Story<PaperProps> = (args) => <Paper {...args} />;
+
+export const Default = Template.bind({});
