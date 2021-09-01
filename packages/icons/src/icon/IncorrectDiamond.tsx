c3f41eea264bc24c27004e74108cec76978b235f
David Larsen <davidplarsen@gmail.com>
2021-09-01 08:51:45 -0500

    Add four diamond icons

---
+++ packages/icons/src/icon/IncorrectDiamond.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { IconProps } from "../types";
+
+const IncorrectDiamond: React.FC<IconProps> = ({
+	className,
+	color = "#FA0660",
+	style,
+}) => {
+	return (
+		<svg
+			width="48"
+			height="48"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			className={className}
+			style={style}
+		>
+			<path
+				d="M21 1.732a6 6 0 016 0l14.7846 8.5359a6 6 0 013 5.1962v17.0718a6 6 0 01-3 5.1962L27 46.2679a6 6 0 01-6 0L6.2154 37.7321a6 6 0 01-3-5.1962V15.4641a6 6 0 013-5.1962L21 1.7321z"
+				fill={color}
+				stroke="#fff"
+			/>
+			<circle cx="24" cy="24" r="13" fill="#fff" />
+			<path
+				d="M19 29l10-10M29 29L19 19"
+				stroke={color}
+				stroke-width="3"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+};
+
+export default IncorrectDiamond;
