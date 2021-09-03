adda2794efa510d57d14a0afea5dc22a8120b11e
Eric Stout <ericwstout@gmail.com>
2021-09-03 15:02:51 -0500

    feat(paper): Add Paper variant that adds logo heading

--- packages/core/src/Paper/index.tsx
+++ packages/core/src/Paper/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
 import classNames from "classnames";
+
+import logo from "./logo.svg";
+
 import { DefaultTheme, PaperProps } from "../types";
+
+const logoHeadingHeight = 52;
 
 const useStyles = createUseStyles((theme: DefaultTheme) => ({
   root: {
@@ -11,9 +16,35 @@ const useStyles = createUseStyles((theme: DefaultTheme) => ({
     height: "auto",
     padding: (props: PaperProps) =>
       props.padding ? props.padding : theme.spacing.base * 4,
-    boxShadow: "0px 5px 20px rgba(0, 0, 0, 0.15)",
-    borderRadius: theme.spacing.base * 2,
-    border: "1px solid #f1f1f1",
+    boxShadow: "0px 5px 20px rgba(0, 0, 0, 0.08)",
+    borderRadius: theme.spacing.base * 3,
+    border: "1px solid #EAEAEB",
+    paddingTop: ({ withLogo, padding }) =>
+      withLogo
+        ? logoHeadingHeight + (padding ? padding : theme.spacing.base * 4)
+        : padding
+        ? padding
+        : theme.spacing.base * 4,
+  },
+  logoHeading: {
+    display: "flex",
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: logoHeadingHeight,
+    backgroundColor: theme.colors.primary.base,
+    textAlign: "center",
+    borderTopLeftRadius: theme.spacing.base * 3,
+    borderTopRightRadius: theme.spacing.base * 3,
+    "& img": {
+      display: "inline-block",
+      position: "relative",
+      width: 100,
+      height: "auto",
+    },
   },
 }));
 
@@ -22,10 +53,18 @@ const Paper: React.FC<PaperProps> = ({
   children,
   style,
   className,
+  withLogo,
 }) => {
-  const classes = useStyles({ padding });
+  const classes = useStyles({ padding, withLogo });
   return (
     <div className={classNames(classes.root, className)} style={style}>
+      {withLogo ? (
+        <div className={classes.logoHeading}>
+          <div>
+            <img src={logo} alt="PickUp" role="presentation" />
+          </div>
+        </div>
+      ) : null}
       {children}
     </div>
   );
