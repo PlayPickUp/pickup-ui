8c10a82109e8a2e4ddcb097a6ce27b3b6407fb80
David Larsen <54251395+dLars99@users.noreply.github.com>
2021-09-28 18:59:11 -0500

    chore(testing): Add tests for FormError (#162)

---
+++ packages/core/src/FormError/FormError.test.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import ThemeProvider from "../ThemeProvider";
+import FormError from "../FormError";
+
+const testErrors = { formError: "This error should render", noError: null };
+
+test("Renders without crashing, matches snapshot", () => {
+  const { container } = render(
+    <ThemeProvider>
+      <FormError
+        errors={testErrors}
+        touched={{ formError: true }}
+        name={"formError"}
+      />
+    </ThemeProvider>
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test("Renders correct error message for name", () => {
+  const { getByText } = render(
+    <ThemeProvider>
+      <FormError
+        errors={testErrors}
+        touched={{ formError: true }}
+        name={"formError"}
+      />
+    </ThemeProvider>
+  );
+  expect(getByText("This error should render")).toBeTruthy();
+});
+
+test("Does not render when no error present", () => {
+  const { queryByTestId } = render(
+    <ThemeProvider>
+      <FormError
+        errors={testErrors}
+        touched={{ noError: true }}
+        name={"noError"}
+      />
+    </ThemeProvider>
+  );
+  expect(queryByTestId("formError")).toBeNull();
+});
+
+test("Does not render when untouched", () => {
+  const { queryByTestId } = render(
+    <ThemeProvider>
+      <FormError
+        errors={testErrors}
+        touched={{ noError: false }}
+        name={"formError"}
+      />
+    </ThemeProvider>
+  );
+  expect(queryByTestId("formError")).toBeNull();
+});
