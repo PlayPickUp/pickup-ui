91a6f701708b72cda0af73413ae1cc3660a0e9aa
David Larsen <davidplarsen@gmail.com>
2021-11-01 08:40:16 -0500

    Add props test

--- packages/core/src/MultiSelect/MultiSelect.test.tsx
+++ packages/core/src/MultiSelect/MultiSelect.test.tsx
@@ -33,8 +33,7 @@ const items: SelectItem[] = [
 
 const FormComponent: React.FC<{
   onSubmit: jest.Mock;
-  initialValues: string[];
-}> = ({ onSubmit, initialValues }) => {
+}> = ({ onSubmit }) => {
   const handleSubmit = async (values) => {
     await sleep(500);
     onSubmit(values);
@@ -44,7 +43,7 @@ const FormComponent: React.FC<{
       <div>
         <Formik
           initialValues={{
-            selectMany: initialValues,
+            selectMany: "",
           }}
           onSubmit={handleSubmit}
         >
@@ -65,19 +64,32 @@ const FormComponent: React.FC<{
 };
 
 const handleSubmit = jest.fn();
+// We could break these out into a testHelpers file, if they're reused elsewhere
 const downArrow = { key: "Down", code: "Down", charCode: 40 };
 const enterKey = { key: "Enter", code: "Enter", charCode: 13 };
 
 test("MultiSelect renders without crashing, matches snapshot", () => {
-  const { container } = render(
-    <FormComponent onSubmit={handleSubmit} initialValues={[]} />
-  );
+  const { container } = render(<FormComponent onSubmit={handleSubmit} />);
   expect(container).toMatchSnapshot();
+});
+
+test("Props are passed and rendered", () => {
+  const { getByText, getByPlaceholderText } = render(
+    <FormComponent onSubmit={handleSubmit} />
+  );
+
+  expect(getByText("MultiSelect Test")).toBeTruthy();
+
+  const input = getByPlaceholderText("Search & Select items...");
+  // Down arrow opens menu
+  fireEvent.keyDown(input, downArrow);
+  expect(getByText("Option 2")).toBeTruthy();
+  cleanup;
 });
 
 test("Selected item gets added to list", () => {
   const { container, getByPlaceholderText } = render(
-    <FormComponent onSubmit={handleSubmit} initialValues={[]} />
+    <FormComponent onSubmit={handleSubmit} />
   );
 
   const input = getByPlaceholderText("Search & Select items...");
@@ -88,12 +100,26 @@ test("Selected item gets added to list", () => {
   cleanup;
 });
 
+test("Props are passed and rendered", () => {
+  const { getByText, getByPlaceholderText } = render(
+    <FormComponent onSubmit={handleSubmit} />
+  );
+
+  expect(getByText("MultiSelect Test")).toBeTruthy();
+
+  const input = getByPlaceholderText("Search & Select items...");
+  // Down arrow opens menu
+  fireEvent.keyDown(input, downArrow);
+  expect(getByText("Option 2")).toBeTruthy();
+  cleanup;
+});
+
+/* TO DO: Test remove button */
 /* Cannot get the button click to work */
 // test("Clicking item button removes it from the list", async () => {
-//   const { getByPlaceholderText, getByText, getByRole } = render(
+//   const { getByPlaceholderText, getByText } = render(
 //     <FormComponent
 //       onSubmit={handleSubmit}
-//       initialValues={["Option 1", "Option 2"]}
 //     />
 //   );
 
@@ -108,12 +134,14 @@ test("Selected item gets added to list", () => {
 //   });
 
 //   act(() => {
-//     fireEvent.click(getByRole("button", { name: /remove/i }));
+//     fireEvent.click(getByText("Option 1").nextElementSibling);
 //   });
 
 //   await waitFor(() => {
 //     fireEvent.blur(input);
 //   });
 
-//   expect(getByText("Option 1")).toBeFalsy();
+//   await waitFor(() => {
+//     expect(getByText("Option 1")).toBeFalsy();
+//   });
 // });
