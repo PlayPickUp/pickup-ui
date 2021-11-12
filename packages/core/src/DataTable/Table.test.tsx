04b192f6b9046269f8ab9c7f1dca6f59fed7d113
David Larsen <davidplarsen@gmail.com>
2021-11-12 16:46:27 -0600

    Add test for data props

--- packages/core/src/DataTable/Table.test.tsx
+++ packages/core/src/DataTable/Table.test.tsx
@@ -6,16 +6,61 @@ import DataTable from ".";
 
 import { rows, headCells } from "./__mocks__/data.mock";
 
+const ActionToolbar = () => {
+  return <div>Edit</div>;
+};
+
+// Cannot snapshot DataTable due to dynamic MUI class names
+
 test("Table title renders correctly", () => {
   const { getByText } = render(
     <ThemeProvider>
       <DataTable
         rows={rows}
-        headCells={headCells as any}
+        headCells={headCells}
         defaultSortColumn={"id"}
         tableTitle="Homebase Posts"
+        actionToolbar={ActionToolbar}
       />
     </ThemeProvider>
   );
   expect(getByText("Homebase Posts")).toBeTruthy();
+});
+
+test("Default sort renders correctly", () => {
+  const { getByText } = render(
+    <ThemeProvider>
+      <DataTable
+        rows={rows}
+        headCells={headCells}
+        defaultSortColumn={"id"}
+        tableTitle="Homebase Posts"
+        actionToolbar={ActionToolbar}
+      />
+    </ThemeProvider>
+  );
+  expect(getByText("ID").childNodes[1].textContent).toEqual(
+    "sorted descending"
+  );
+});
+
+test("Data is received and rendered", () => {
+  const { getByText } = render(
+    <ThemeProvider>
+      <DataTable
+        rows={rows}
+        headCells={headCells}
+        defaultSortColumn={"id"}
+        tableTitle="Homebase Posts"
+        actionToolbar={ActionToolbar}
+      />
+    </ThemeProvider>
+  );
+
+  expect(getByText("League")).toBeTruthy();
+  expect(getByText("MLB")).toBeTruthy();
+  expect(getByText("Title")).toBeTruthy();
+  expect(
+    getByText("Ranking the Cleveland Baseball team's outfield power")
+  ).toBeTruthy();
 });
