ce5d488d42806d52693f60c5e4e0c0124f8da122
David Larsen <davidplarsen@gmail.com>
2021-11-16 09:11:36 -0600

    Change unknown to any to match react-router Link type

--- packages/core/src/Chip/Chip.test.tsx
+++ packages/core/src/Chip/Chip.test.tsx
@@ -1,4 +1,4 @@
-import React, { JSXElementConstructor } from "react";
+import React from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { fireEvent, render } from "@testing-library/react";
 
