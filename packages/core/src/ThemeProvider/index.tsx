6725474055ffb91090bce10a6ad1082cb4d02c78
David Larsen <davidplarsen@gmail.com>
2021-11-19 13:55:13 -0600

    Deprecate aggressive prop

--- packages/core/src/ThemeProvider/index.tsx
+++ packages/core/src/ThemeProvider/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { jss, JssProvider, ThemeProvider as JSSThemeProvider } from "react-jss";
-import { increaseSpecificity } from "./specificityPlugin";
 import { containUI } from "./containerPlugin";
 import merge from "lodash/merge";
 
@@ -13,17 +12,15 @@ const ThemeProvider: React.FC<ThemeProviderProps> = ({
   children,
   withReset = true,
   theme,
-  aggressive = false,
   ...rest
 }) => {
   jss.use(containUI());
-  if (aggressive) jss.use(increaseSpecificity());
   const mergedTheme = theme ? merge(defaultTheme, theme) : defaultTheme;
   return (
     <div id="PickUpUI">
       <JssProvider jss={jss} classNamePrefix="PU--">
         <JSSThemeProvider theme={mergedTheme} {...rest}>
-            {withReset ? <GlobalsAndReset>{children}</GlobalsAndReset> : children}
+          {withReset ? <GlobalsAndReset>{children}</GlobalsAndReset> : children}
         </JSSThemeProvider>
       </JssProvider>
     </div>
