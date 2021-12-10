afbfb437103a5b4243f7da993471eb967df22733
David Larsen <davidplarsen@gmail.com>
2021-12-10 15:51:13 -0600

    test(horizontalRule): Enhance HorizontalRule tests

--- packages/core/src/HorizontalRule/HorizontalRule.test.tsx
+++ packages/core/src/HorizontalRule/HorizontalRule.test.tsx
@@ -21,3 +21,28 @@ test("Horizontal Rule with Bolt renders without crashing, matches snapshot", () 
   );
   expect(container).toMatchSnapshot();
 });
+
+test("Color prop changes the color of the Horizontal Rule and bolt", () => {
+  const { container } = render(
+    <ThemeProvider>
+      <HorizontalRule color="red" showBolt />
+    </ThemeProvider>
+  );
+  expect(container).toMatchSnapshot();
+});
+
+test("Props are passed and rendered", () => {
+  const { getByTestId } = render(
+    <ThemeProvider>
+      <HorizontalRule className="test-1-2-3" style={{ opacity: 0.5 }} />
+    </ThemeProvider>
+  );
+  // className
+  expect(getByTestId("horizontalRule").getAttribute("class")).toContain(
+    "test-1-2-3"
+  );
+  // style
+  expect(getByTestId("horizontalRule").getAttribute("style")).toContain(
+    "opacity: 0.5"
+  );
+});
