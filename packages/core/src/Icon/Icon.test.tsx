7f1a0bee18a93c7c902fe68634c61ac1a2d1b79a
David Larsen <54251395+dLars99@users.noreply.github.com>
2021-12-20 08:28:38 -0600

    test(icon): Add tests for icon props (#265)

--- packages/core/src/Icon/Icon.test.tsx
+++ packages/core/src/Icon/Icon.test.tsx
@@ -27,13 +27,46 @@ test("Child component is passed and rendered correctly", () => {
   expect(getByTestId("icon").hasChildNodes()).toBeTruthy();
 });
 
-test("Style object is passed and rendered correctly", () => {
+test("Style props are passed and rendered correctly", () => {
   const { getByTestId } = render(
     <ThemeProvider>
-      <Icon style={{ position: "absolute" }}>
+      <Icon className="test-1-2-3" style={{ position: "absolute" }}> 
         <Create />
       </Icon>
     </ThemeProvider>
   );
   expect(getByTestId("icon").hasAttribute("style")).toBeTruthy();
+  expect(getByTestId("icon").getAttribute("class")).toContain("test-1-2-3");
 });
+
+test("Color prop renders correctly", () => {
+  const { container } = render(
+    <ThemeProvider>
+      <Icon color="#123456"> 
+        <Create />
+      </Icon>
+    </ThemeProvider>
+  );
+  const pathElements = container.getElementsByTagName("path");
+  let containsColor = false;
+  for (const el of pathElements) {
+    if (el.getAttribute("stroke") === "#123456") {
+      containsColor = true;
+      break;
+    }
+  }
+  expect(containsColor).toBeTruthy();
+});
+
+test("Size prop renders correctly", () => {
+  const { getByTestId } = render(
+    <ThemeProvider>
+      <Icon size={10}> 
+        <Create />
+      </Icon>
+    </ThemeProvider>
+  );
+    const style = window.getComputedStyle(getByTestId("icon"));
+    expect(style.width).toBe("10px");
+})
+
