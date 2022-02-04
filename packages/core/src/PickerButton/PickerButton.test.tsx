b6c17c349049176e21d82fcdc4da9c85ac5e6536
David Larsen <davidplarsen@gmail.com>
2022-02-04 12:02:22 -0600

    test(pickerButton): Enhance PickerButton tests

--- packages/core/src/PickerButton/PickerButton.test.tsx
+++ packages/core/src/PickerButton/PickerButton.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { fireEvent, render } from "@testing-library/react";
-import jssSerializer from 'jss-snapshot-serializer';
+import jssSerializer from "jss-snapshot-serializer";
 expect.addSnapshotSerializer(jssSerializer);
 import ThemeProvider from "../ThemeProvider";
 import PickerButton from "./index";
@@ -87,4 +87,81 @@ test("Display of results text renders correctly", () => {
   );
   expect(getByText("Absolutely")).toBeTruthy();
   expect(getByText("85%")).toBeTruthy();
+});
+
+test("Result does not show if showResult is false", () => {
+  const { queryByText } = render(
+    <ThemeProvider>
+      <PickerButton
+        showResult={false}
+        isPick={true}
+        result={85}
+        displayText="Absolutely"
+        onClick={handleClick}
+      />
+    </ThemeProvider>
+  );
+  expect(queryByText("85%")).toBeFalsy();
+});
+
+test("className and style props are passed and rendered correctly", () => {
+  const { getByRole } = render(
+    <ThemeProvider>
+      <PickerButton
+        result={74}
+        displayText="Absolutely"
+        isPick={false}
+        onClick={handleClick}
+        className="test-12345"
+        style={{ margin: "10px" }}
+      />
+    </ThemeProvider>
+  );
+
+  const button = getByRole("button");
+  expect(button.getAttribute("class")).toContain("test-12345");
+  const style = window.getComputedStyle(button);
+  expect(style.margin).toBe("10px");
+});
+
+test("isPick renders its class", () => {
+  const { getByRole } = render(
+    <ThemeProvider>
+      <PickerButton
+        result={74}
+        displayText="Absolutely"
+        isPick={true}
+        onClick={handleClick}
+      />
+    </ThemeProvider>
+  );
+  expect(getByRole("button").getAttribute("class")).toContain("resultPicked");
+});
+
+test("isCorrect renders its class", () => {
+  const { getByRole } = render(
+    <ThemeProvider>
+      <PickerButton
+        result={74}
+        displayText="Absolutely"
+        isCorrect={true}
+        onClick={handleClick}
+      />
+    </ThemeProvider>
+  );
+  expect(getByRole("button").getAttribute("class")).toContain("isCorrect");
+});
+
+test("isIncorrect renders its class", () => {
+  const { getByRole } = render(
+    <ThemeProvider>
+      <PickerButton
+        result={74}
+        displayText="Absolutely"
+        isIncorrect={true}
+        onClick={handleClick}
+      />
+    </ThemeProvider>
+  );
+  expect(getByRole("button").getAttribute("class")).toContain("isIncorrect");
 });
