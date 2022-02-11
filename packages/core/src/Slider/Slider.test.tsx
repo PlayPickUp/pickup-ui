12e628ad555adfb8778b7c364bbb90f8d7311b42
Krystine Jannenga <90875462+KrystineJannenga@users.noreply.github.com>
2022-02-11 11:43:00 -0700

    updated styling w/ prettier

--- packages/core/src/Slider/Slider.test.tsx
+++ packages/core/src/Slider/Slider.test.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import jssSerializer from 'jss-snapshot-serializer';
+import jssSerializer from "jss-snapshot-serializer";
 expect.addSnapshotSerializer(jssSerializer);
 import ThemeProvider from "../ThemeProvider";
 import Slider from "./index";
 
 test("Renders and matches snapshot", () => {
-    const { container } = render(
-      <ThemeProvider>
-        <Slider>
-            <div></div>
-        </Slider>
-      </ThemeProvider>
-    );
-    expect(container).toMatchSnapshot();
-  });
+  const { container } = render(
+    <ThemeProvider>
+      <Slider>
+        <div></div>
+      </Slider>
+    </ThemeProvider>
+  );
+  expect(container).toMatchSnapshot();
+});
 
 test("Slider children are passed and rendered", () => {
-    const { getByText } = render(
-        <ThemeProvider>
-            <Slider> <div>Hello, World</div></Slider>
-        </ThemeProvider>
-    );
-    expect(getByText("Hello, World")).toBeTruthy();
+  const { getByText } = render(
+    <ThemeProvider>
+      <Slider>
+        <div>Hello, World</div>
+      </Slider>
+    </ThemeProvider>
+  );
+  expect(getByText("Hello, World")).toBeTruthy();
 });
